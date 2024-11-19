0afb6276e8586da257afe6889a52f58c94ee1f94
kalios01 <priyanshu07893@gmail.com>
2024-11-19 05:43:42 +0530

    intial

---
+++ ProView/app/src/main/java/com/kaliostech/proview/onboarding/screens/ThirdScreen.kt
@@ -0,0 +1,37 @@
+package com.kaliostech.proview.onboarding.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kaliostech.proview.R
+import android.content.Context
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+class ThirdScreen : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_third_screen, container, false)
+
+        view.findViewById<Button>(R.id.finalscreenbtn).setOnClickListener {
+            findNavController().navigate(R.id.action_viewPagerFragment_to_homeFragment)
+            onBoardingFinished()
+        }
+
+        return view
+    }
+
+    private fun onBoardingFinished(){
+        val sharedPref = requireActivity().getSharedPreferences("onBoarding", Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        editor.putBoolean("Finished", true)
+        editor.apply()
+    }
+
+}
